ea269f6ec58ba21b81ca5aeeabf1fb7c1656aaf3
Jaehyeon Kim <202580061+jaykxo@users.noreply.github.com>
2025-04-02 11:32:24 +0900

    Title: 친근하지 않은 수, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250402/친근하지 않은 수/unfriendly-number.py
+++ 250402/친근하지 않은 수/unfriendly-number.py
@@ -2,7 +2,7 @@ N = int(input())
 unfam = []
 
 for i in range(1, N+1):
-    if i % 2 = 0 or i % 3 = 0 or i % 5 =. :
+    if i % 2 == 0 or i % 3 == 0 or i % 5 == 0:
         continue
     unfam.append(i)
-    print(count(num))
+print(len(unfam))
