76e50da70d38f8c1067b5e9728d7b40162b4f301
Jaehyeon Kim <202580061+jaykxo@users.noreply.github.com>
2025-04-07 23:21:40 +0900

    Title: 정수의 최솟값, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250407/정수의 최솟값/minimum-value-of-an-integer.py
+++ 250407/정수의 최솟값/minimum-value-of-an-integer.py
@@ -3,9 +3,9 @@ a, b, c = map(int, input().split())
 def minimum(a, b, c):
     min_value = a
     if min_value > b:
-        b = min_value
+        min_value = b
     if min_value > c:
-        c = min_value
+        min_value = c
     print(min_value)
     
 minimum(a, b, c)
