8e7f13093540ceb7dba98823a5fb0465a47cbe24
Jaehyeon Kim <jkxojkxo@gmail.com>
2025-09-10 22:00:10 +0900

    Title: 4가지 관계연산자, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

--- 250910/4가지 관계연산자/four-relational-operators.js
+++ 250910/4가지 관계연산자/four-relational-operators.js
@@ -4,5 +4,6 @@ let input = fs.readFileSync(0).toString().trim().split(" ");
 let A = Number(input[0]), B = Number(input[1]);
 
 console.log(A >= B ? 1 : 0);
-console.log(A >= B ? 1 : 0);
-console.log(A >= B ? 1 : 0);
+console.log(A > B ? 1 : 0);
+console.log(A <= B ? 1 : 0);
+console.log(A < B ? 1 : 0);
