31d0416ed69cf2a1b40b930be7ca9b5d3d4ce4be
Jaehyeon Kim <jkxojkxo@gmail.com>
2025-09-10 20:01:21 +0900

    Title: Calculating the Sum and Average of Three Numbers, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250910/Calculating the Sum and Average of Three Numbers/sum-and-mean-of-three-numbers.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let A = Number(input[0]), B = Number(input[1]), C = Number(input[2]);
+
+console.log(A + B + C);
+console.log(parseInt((A + B + C)/3));
