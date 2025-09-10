074e06a49499583e81951db33507a80945acbe7b
Jaehyeon Kim <jkxojkxo@gmail.com>
2025-09-10 20:26:49 +0900

    Title: 체질량지수, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250910/체질량지수/bmi.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let h = Number(input[0]);
+let w = Number(input[1]);
+
+let b = Math.floor((10000 * w) / h ** 2);
+console.log(b);
+
+if (b >= 25) {
+    console.log("Obesity");
+};
