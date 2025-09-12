c04e3811ca168296f77f80bac18996762e32acbe
Jaehyeon Kim <jkxojkxo@gmail.com>
2025-09-12 17:16:11 +0900

    Title: 장학금, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250912/장학금/scholarship.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let mid = Number(input[0]), fin = Number(input[1]);
+
+if (mid < 90) {
+    console.log(0)
+} else if (fin >= 95) {
+    console.log(100000)
+} else if (fin >= 90) {
+    console.log(50000)
+} else {
+    console.log(0)
+}
