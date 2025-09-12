fe8d2536ce9c6a683556dd5f3a78df393173cf17
Jaehyeon Kim <jkxojkxo@gmail.com>
2025-09-12 17:41:12 +0900

    Title: 두 사람, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

--- 250912/두 사람/two-person.js
+++ 250912/두 사람/two-person.js
@@ -1,13 +1,11 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split("\n");
 
-let A = input[0].split(" ").map(Number);
-let B = input[1].split(" ").map(Number);
+let A = input[0].split(" ").map(String);
+let B = input[1].split(" ").map(String);
 
-if(A[1] === "W" && B[1] === "W") {
-    console.log(0)
-} else if (A[0] < 19 && B[0] < 19) {
-    console.log(0)
-} else {
+if((+A[0] >= 19 && A[1] === "M") || (+B[0] >= 19 && B[1] === "M")) {
     console.log(1)
+}  else {
+    console.log(0)
 };
