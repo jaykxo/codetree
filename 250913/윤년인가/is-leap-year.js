f8d55e1188b09f899d0c8b3a106e0c55adb436d9
Jaehyeon Kim <jkxojkxo@gmail.com>
2025-09-13 19:33:03 +0900

    Title: 윤년인가, Time: 71ms, Memory: 10MB, Status: Passed - Codetree

--- 250913/윤년인가/is-leap-year.js
+++ 250913/윤년인가/is-leap-year.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 let Y = Number(fs.readFileSync(0).toString());
 
-if (Y % 4 != 0) {
-    console.log("false")
-} else if (Y % 400 === 0) {
-    console.log("true")
-} else if (Y % 100 === 0) {
-    console.log("false")
+if (Y % 4 === 0) {
+    if(Y % 100 === 0 && Y % 400 != 0) {
+        console.log("false")
+    } else {
+        console.log("true")
+    }
 } else {
-    console.log("true")
-}
+    console.log("false")
+};
