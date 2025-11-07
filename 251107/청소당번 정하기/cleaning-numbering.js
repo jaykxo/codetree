818d664b1fc058c42d41e1725c088081469c9ffa
Jaehyeon Kim <jkxojkxo@gmail.com>
2025-11-07 17:49:25 +0900

    Title: 청소당번 정하기, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251107/청소당번 정하기/cleaning-numbering.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString());
+
+let classroom = 0;
+let hallway = 0;
+let toilet = 0;
+
+for (let i = 1; i <= n; i++) {
+    if (i % 12 == 0) {
+        toilet++
+    } else if (i % 3 == 0) {
+        hallway++
+    } else if (i % 2 == 0) {
+        classroom++
+    }
+};
+
+console.log(classroom, hallway, toilet);
