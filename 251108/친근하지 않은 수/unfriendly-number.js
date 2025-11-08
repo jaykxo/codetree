c0c56bccd4458e0e4ac443a2f03edbefa688588a
Jaehyeon Kim <jkxojkxo@gmail.com>
2025-11-08 21:24:26 +0900

    Title: 친근하지 않은 수, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251108/친근하지 않은 수/unfriendly-number.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+let N = Number(fs.readFileSync(0).toString());
+let cnt = 0;
+
+for (let i = 1; i <= N; i++) {
+    if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0) {
+        continue
+    } else {
+        cnt++
+    }
+};
+
+console.log(cnt);
