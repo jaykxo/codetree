e72286d79a4910abe02e54dfc4b266c084a707b0
Jaehyeon Kim <jkxojkxo@gmail.com>
2025-11-24 17:44:39 +0900

    Title: 십의 자리 숫자, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251124/십의 자리 숫자/number-of-tens-digit.py
+++ 251124/십의 자리 숫자/number-of-tens-digit.py
@@ -2,13 +2,12 @@ import sys
 input = sys.stdin.readline
 
 arr = list(map(int, input().split()))
-
-for i in range(len(arr)):
-    arr[i] //= 10
-
 cnt = [0] * 10
 
 for x in arr:
+    if x == 0:
+        break
+    x //= 10
     cnt[x] += 1
 
 for i in range(1, 10):
