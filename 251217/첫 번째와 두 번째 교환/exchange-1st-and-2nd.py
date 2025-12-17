3c5141eac5faf16f5e17ab8ca6d2b34123939e2f
Jaehyeon Kim <jkxojkxo@gmail.com>
2025-12-17 23:59:06 +0900

    Title: 첫 번째와 두 번째 교환, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251217/첫 번째와 두 번째 교환/exchange-1st-and-2nd.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+word = list(map(str, input().strip()))
+f = word[0]
+s = word[1]
+
+for i in range(len(word)):
+    if word[i] == f:
+        word[i] = s
+    elif word[i] == s:
+        word[i] = f
+
+print(''.join(map(str, word)))
