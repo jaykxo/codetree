ba8e85a65d463475266dfa1f02de4f4cc5f0d6dd
Jaehyeon Kim <jkxojkxo@gmail.com>
2025-12-18 23:33:18 +0900

    Title: 문자 제거하기, Time: 78ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251218/문자 제거하기/del-char.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+word = list(map(str, input().strip()))
+
+for _ in range(len(word) - 1):
+    n = int(input())
+    if n <= len(word):
+        word.pop(n)
+    else:
+        word.pop(-1)
+
+    print(''.join(map(str, word)))
