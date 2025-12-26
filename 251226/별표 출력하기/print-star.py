41da04e58a54b366bd4cec2f7fa02c684b935236
Jaehyeon Kim <jkxojkxo@gmail.com>
2025-12-26 23:21:12 +0900

    Title: 별표 출력하기, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251226/별표 출력하기/print-star.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+for i in range(N - 1):         
+    for j in range(i + 1): 
+        print("*", end=" ")
+    print()
+    
+for i in range(N):         
+    for j in range(N - i): 
+        print("*", end=" ")
+    print()
