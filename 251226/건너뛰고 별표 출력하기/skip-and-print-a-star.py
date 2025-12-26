12324b62b61ec572958ca1a0d6dfa36a3eaed3a5
Jaehyeon Kim <jkxojkxo@gmail.com>
2025-12-26 23:25:53 +0900

    Title: 건너뛰고 별표 출력하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251226/건너뛰고 별표 출력하기/skip-and-print-a-star.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+for i in range(N - 1):         
+    for j in range(i + 1): 
+        print("*", end="")
+    print()
+    print()
+
+    
+for i in range(N):         
+    for j in range(N - i): 
+        print("*", end="")
+    print()
+    print()
