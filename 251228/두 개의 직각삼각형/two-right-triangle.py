e5cbd7755b915f1ca5272d1a87344dda2c09fee2
Jaehyeon Kim <jkxojkxo@gmail.com>
2025-12-28 23:36:46 +0900

    Title: 두 개의 직각삼각형, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251228/두 개의 직각삼각형/two-right-triangle.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+for i in range(N):
+    for j in range(N - i):
+        print("*", end="")
+    for j in range(i * 2):
+        print(" ", end="")
+    for j in range(N - i):
+        print("*", end="")
+    print()
