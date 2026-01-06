0a17e33c6aef6c857ef30ed504bdd5861cac887a
Jaehyeon Kim <jkxojkxo@gmail.com>
2026-01-06 16:30:27 +0900

    Title: 007, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260106/007/007.py
@@ -0,0 +1,8 @@
+secret_code, meeting_point, time = input().split()
+time = int(time)
+
+# Please write your code here.
+
+print("secret code :", secret_code)
+print("meeting point :", meeting_point)
+print("time :", time)
